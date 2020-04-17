9cb9a360505b3267874894bf7c8dc259baae70b2
beniciovargas <beniciorvargas@gmail.com>
2020-04-17 15:03:22 -0700

    adjusts logic for profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -19,37 +19,41 @@ export default class Profile extends React.Component{
         friends:[]
       }
     
-      componentDidMount() {
-       
-        if (localStorage.jwtToken) {
-          setAuthHeader(localStorage.jwtToken);
-          const decoded = jwt_decode(localStorage.getItem('jwtToken'));
-          this.setState({
-            user: decoded.username,
-            userId: decoded._id
-          })
-        }
-          
-        axios.get(`http://localhost:4000/api/v1/users/${this.state.userId}`)
-        .then((res) => {
-          console.log(res)
-          this.setState({
-            nextUpShows: res.data[0].nextUp,
-            currentlyWatchingShows: res.data[0].currentlyWatching,
-            faveShows: res.data[0].faves,
-            friends: res.data[0].friends
-          })
+    componentDidMount() {
+      
+      if (localStorage.jwtToken) {
+        setAuthHeader(localStorage.jwtToken);
+        const decoded = jwt_decode(localStorage.getItem('jwtToken'));
+        this.setState({
+          user: decoded.username,
+          userId: decoded._id
         })
-        .catch((err)=> {console.log(err)})
       }
+        
+      axios.get(`http://localhost:4000/api/v1/users/${this.state.userId}`)
+      .then((res) => {
+        console.log(res)
+        this.setState({
+          nextUpShows: res.data[0].nextUp,
+          currentlyWatchingShows: res.data[0].currentlyWatching,
+          faveShows: res.data[0].faves,
+          friends: res.data[0].friends
+        })
+      })
+      .catch((err)=> {console.log(err)})
+    }
 
     render(){
       
         let nextUpShows = this.state.nextUpShows.map((show)=>{
           if (this.state.nextUpShows.length>0){
             return (<NextUp show= {show}
-                            key={show._id} />)}
-        })
+                            key={show._id} />)
+          }else{
+            return (<h1>search to add new shows!</h1>)
+          }
+        }) 
+
         let currentlyWatchingShows = this.state.currentlyWatchingShows.map((show)=>{
           if (this.state.currentlyWatchingShows.length>0){
             return (<CurrentlyWatching show= {show}
@@ -65,6 +69,7 @@ export default class Profile extends React.Component{
             return (<Friends friend= {friend}
                             key={friend._id} />)}
         })
+
         return(
             <div className = "profile-header">
                 <div className = "profile-header">
